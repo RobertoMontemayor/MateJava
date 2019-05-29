b39237ac2a2a1cb26e9242a78c510c048177f9ad
Roberto Montemayor <roberto.montemayor.developer@gmail.com>
2019-05-28 21:04:17 -0500

    Le puse un comentario

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 class Main{
-   public static void main(String args[]){
+   public static void main(String args[]){///Comentario
       int resp = Lectura.readInt("Ingresa lo que quieres hacer \n1=Multi\2=Suma");
    }
 }
